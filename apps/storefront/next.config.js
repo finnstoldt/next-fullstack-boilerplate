b1b89b76e9ea45da73f523462ac4f2ee390a8044
Finn Stoldt <stoldtf@gmail.com>
2022-04-12 13:02:53 +0200

    chore(storefront): create dockerfile

--- apps/storefront/next.config.js
+++ apps/storefront/next.config.js
@@ -1,7 +1,10 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
+const path = require("path");
+const withTM = require("next-transpile-modules")(["ui", "logger"]);
+
+module.exports = withTM({
   reactStrictMode: true,
   experimental: {
     outputStandalone: true,
+    outputFileTracingRoot: path.join(__dirname, "../../"),
   },
-};
+});
