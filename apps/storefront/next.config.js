58a8be17ecc64065515a221778b8e4ed3cd087fb
Finn Stoldt <stoldtf@gmail.com>
2022-04-14 13:28:19 +0200

    chore(storefront): add basePath paramter to next config

--- apps/storefront/next.config.js
+++ apps/storefront/next.config.js
@@ -1,8 +1,12 @@
 const path = require("path");
 const withTM = require("next-transpile-modules")(["ui", "logger"]);
 
+/**
+ * @type {import('next').NextConfig}
+ */
 module.exports = withTM({
   reactStrictMode: true,
+  basePath: process.env.BASE_PATH || "/",
   experimental: {
     outputStandalone: true,
     outputFileTracingRoot: path.join(__dirname, "../../"),
