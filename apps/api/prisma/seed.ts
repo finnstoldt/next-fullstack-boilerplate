c31fa7be9c82f0a2a77250928d6b2a1276be7f1f
Finn Stoldt <stoldtf@gmail.com>
2022-04-22 13:20:47 +0200

    feat(api): add prisma

---
+++ apps/api/prisma/seed.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function main() {
+  const alice = await prisma.user.upsert({
+    where: { email: "alice@prisma.io" },
+    update: {},
+    create: {
+      email: "alice@prisma.io",
+      name: "Alice",
+      role: "ADMIN",
+    },
+  });
+
+  const bob = await prisma.user.upsert({
+    where: { email: "bob@prisma.io" },
+    update: {},
+    create: {
+      email: "bob@prisma.io",
+      name: "Bob",
+    },
+  });
+  console.log({ alice, bob });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,11 +1,19 @@
 import { createServer } from "./server";
 import { log } from "logger";
+import { PrismaClient } from "@prisma/client";
 
 const port = process.env.PORT || 3000;
 const server = createServer();
+const prisma = new PrismaClient();
 
 server.get("/", function (req, res) {
   res.send("Hello World!");
+});
+
+server.get("/prisma", async function (req, res) {
+  res.send(
+    JSON.stringify(await prisma.user.findMany({ select: { name: true } }))
+  );
 });
 
 server.listen(port, () => {
