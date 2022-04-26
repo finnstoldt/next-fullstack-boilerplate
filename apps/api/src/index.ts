311bf8a13bf9b7e239d9064aef9f07225103fa25
Finn Stoldt <stoldtf@gmail.com>
2022-04-26 17:35:34 +0200

    feat(api): add graphql (apollo server)

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,21 +1,74 @@
-import { createServer } from "./server";
 import { log } from "logger";
 import { PrismaClient } from "@prisma/client";
+import { json, urlencoded } from "body-parser";
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import http from "http";
+
+import { ApolloServer } from "apollo-server-express";
+import { ApolloServerPluginDrainHttpServer, gql } from "apollo-server-core";
 
 const port = process.env.PORT || 3000;
-const server = createServer();
+
 const prisma = new PrismaClient();
 
-server.get("/", function (req, res) {
-  res.send("Hello World!");
+const app = express();
+
+app
+  .disable("x-powered-by")
+  .use(morgan("dev"))
+  .use(urlencoded({ extended: true }))
+  .use(json())
+  .use(cors())
+  .get("/", function (req, res) {
+    res.send("Hello World!");
+  })
+  .get("/prisma", async function (req, res) {
+    res.send(
+      JSON.stringify(await prisma.user.findMany({ select: { name: true } }))
+    );
+  });
+
+const httpServer = http.createServer(app);
+
+const typeDefs = gql`
+  type User {
+    id: Int
+    createdAt: String
+    email: String
+    name: String
+    role: String
+  }
+  type Query {
+    users: [User]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    users: async () =>
+      (await prisma.user.findMany()).map((user) => ({
+        id: user.id,
+        createdAt: user.createdAt.toISOString(),
+        email: user.email,
+        name: user.name,
+        role: user.role.toString(),
+      })),
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-server.get("/prisma", async function (req, res) {
-  res.send(
-    JSON.stringify(await prisma.user.findMany({ select: { name: true } }))
-  );
-});
+async function startServer() {
+  await server.start();
+  server.applyMiddleware({ app });
+  await new Promise<void>((resolve) => httpServer.listen({ port }, resolve));
+  log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`);
+}
 
-server.listen(port, () => {
-  log(`api running on ${port}`);
-});
+startServer();
