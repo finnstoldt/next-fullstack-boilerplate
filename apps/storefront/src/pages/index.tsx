14c289938f3fe352c696a07139727384e619f48b
Finn Stoldt <stoldtf@gmail.com>
2022-04-28 13:49:40 +0200

    feat(storefront): add tailwindcss

--- apps/storefront/src/pages/index.tsx
+++ apps/storefront/src/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Store() {
   log("Hey! This is Home.");
   return (
     <div>
-      <h1>Store</h1>
+      <h1 className="text-3xl font-bold underline">Store</h1>
       <CounterButton />
       <NewTabLink href="https://turborepo.org/">
         Built With TurboRepo
