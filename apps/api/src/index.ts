a496e65440700d0a4fea103302548691e201678b
Finn Stoldt <stoldtf@gmail.com>
2022-04-29 12:54:14 +0200

    fix(api): change gql schema loading method

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -5,12 +5,14 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import http from "http";
-import { readFileSync } from "fs";
+import path from "path";
 
 import { ApolloServer } from "apollo-server-express";
 import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
 
 import { Resolvers } from "graphql-codegen";
+import { loadSchemaSync } from "@graphql-tools/load";
+import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
 
 const port = process.env.PORT || 3000;
 
@@ -35,7 +37,12 @@ app
 
 const httpServer = http.createServer(app);
 
-const typeDefs = readFileSync("./schema.graphql", "utf8");
+const typeDefs = loadSchemaSync(
+  path.resolve(__dirname, "./graphql/schema.graphql"),
+  {
+    loaders: [new GraphQLFileLoader()],
+  }
+);
 
 const resolvers: Resolvers = {
   Query: {
