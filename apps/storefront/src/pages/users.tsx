f7d8aac0b353ce7ad1abdc8c11676b65796438b6
Finn Stoldt <stoldtf@gmail.com>
2022-05-05 16:01:17 +0200

    build: various dependency fixes

--- apps/storefront/src/pages/users.tsx
+++ apps/storefront/src/pages/users.tsx
@@ -1,29 +1,20 @@
 import { log } from "logger";
-import { UserDocument, UserQuery } from "graphql-codegen";
-import client from "../graphql/apollo-client";
+import { useUserQuery } from "graphql-codegen";
 
-export default function Users({ userQuery }: { userQuery: UserQuery }) {
+export default function Users() {
   log("Hey! This is Home.");
+
+  const { data } = useUserQuery();
+
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Users</h1>
       <div className="flex">
-        {userQuery.users?.map((user) => {
-          return <div key={user?.id}>{user?.name}</div>;
-        })}
+        {data &&
+          data.users?.map((user) => {
+            return <div key={user?.id}>{user?.name}</div>;
+          })}
       </div>
     </div>
   );
-}
-
-export async function getStaticProps() {
-  const { data } = await client.query({
-    query: UserDocument,
-  });
-
-  return {
-    props: {
-      userQuery: data,
-    },
-  };
 }
