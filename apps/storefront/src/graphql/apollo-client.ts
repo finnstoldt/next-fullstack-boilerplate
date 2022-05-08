9d1d6c8d699288f24ad35d5b830fda79a40bf7d8
Finn Stoldt <stoldtf@gmail.com>
2022-05-08 19:51:35 +0200

    chore(storefront): log api url

--- apps/storefront/src/graphql/apollo-client.ts
+++ apps/storefront/src/graphql/apollo-client.ts
@@ -1,4 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { log } from "logger";
+
+log("api-url: " + process.env.NEXT_PUBLIC_API_URL);
 
 const client = new ApolloClient({
   uri: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/graphql",
