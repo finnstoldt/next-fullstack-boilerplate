d825620212de6c32afacef0b7ce854be5afd4182
Finn Stoldt <stoldtf@gmail.com>
2022-05-09 09:22:01 +0200

    fix(storefront): inject env var at build time

--- apps/storefront/src/graphql/apollo-client.ts
+++ apps/storefront/src/graphql/apollo-client.ts
@@ -1,10 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { log } from "logger";
-
-log("api-url: " + process.env.NEXT_PUBLIC_API_URL);
 
 const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/graphql",
+  uri:
+    process.env.NEXT_PUBLIC_STOREFRONT_API_URL ||
+    "http://localhost:3000/graphql",
   cache: new InMemoryCache(),
 });
 
