2da2d1c883bf15bdf2b5718e637be37a8e4958f3
Finn Stoldt <stoldtf@gmail.com>
2022-05-10 16:04:00 +0200

    feat(admin): add base path env support

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,6 +1,10 @@
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import { defineConfig } from "vite";
 
+/**
+ * @type {import('vite').UserConfig}
+ */
 export default defineConfig({
   plugins: [reactRefresh()],
+  base: process.env.ADMIN_BASE_PATH,
 });
